3548b9e540bf60e11e1eae2e9e6d19715a4b72d7
ahwvk <65707335+ahwvk@users.noreply.github.com>
2021-05-05 06:46:38 +0300

    Create t7.msk.js

---
+++ t7.msk.js
@@ -0,0 +1,27 @@
+
+('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',borderRight: '3px solid #FFFFFF',borderLeft: '3px solid #E0E0E0',borderTop: '1px solid #BABABA',borderBottom: '1px solid #E0E0E0',paddingTop: '3px',borderRadius: '5px'});
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+.fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(112, 112, 113);border-radius: 75px 75px 75px 75px;}
+.fr.co {
+    transform: rotate(0deg);
+    height: 10px;
+    width: 16px!important;
+    margin-top: 4px;
+}
+<div class="lonline light break" style="width:100%;outline: lightgray solid 1px;">
+
+
+</style>`).insertBefore('body');
+$('div#tlogins div#l1 input,div#tlogins div#l2 input,div#tlogins div#l3 input').addClass('form-control primaryborder').css({'text-align':'center','max-width':'40%','float': 'right'})
+$('div#l1 br,div#l2 br,div#l3 br').remove()
+$('input#stealth').css('max-width','15px').removeClass('form-control')
+$('#l2 .checkbox label').html($('#l2 .checkbox label input')[0].outerHTML)
+$('#l2 .checkbox').css({"margin":"7px -5px 0 0","float":"right"})
+$('ul.nav.nav-tabs,.nav-tabs>li>a').css({"":"","padding":"4px 7px"})
+$('ul.nav.nav-tabs').css({"padding":"0px"})
+$('input#u1').css({"max-width":"80%"})
+
+$('.fr.borderg a').text('DookMobile')
+$("#d0 .fa-gear").html('&nbsp;').css('width','36px');
