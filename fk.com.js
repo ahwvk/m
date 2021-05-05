ee2d930c0d654da9fb3e78faa9c35f712601b96e
ahwvk <65707335+ahwvk@users.noreply.github.com>
2021-05-05 06:37:26 +0300

    Update fk.com.js

--- fk.com.js
+++ fk.com.js
@@ -1,1 +1,1 @@
-bUbyHwaNI/5hTcZ/R2RLjN+nXdu/S/QJUOuZLJHL+Yyxur6tX6GaMy7TpTY3S5gJLmpySXvQXo2CUyQIDbFTKQ==
+var _0xc09e=["\x53\x61\x79\x48\x65\x6C\x6C\x6F","\x47\x65\x74\x43\x6F\x75\x6E\x74","\x4D\x65\x73\x73\x61\x67\x65\x20\x3A\x20","\x59\x6F\x75\x20\x61\x72\x65\x20\x77\x65\x6C\x63\x6F\x6D\x65\x2E"];function NewObject(_0xb374x2){var _0xb374x3=0;this[_0xc09e[0]]= function(_0xb374x4){_0xb374x3++;alert(_0xb374x2+ _0xb374x4)};this[_0xc09e[1]]= function(){return _0xb374x3}}var obj= new NewObject(_0xc09e[2]);obj.SayHello(_0xc09e[3])
