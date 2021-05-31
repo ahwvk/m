0a71c558f29ffb8f621a641bcd15eb4fa26750d4
ahwvk <65707335+ahwvk@users.noreply.github.com>
2021-05-31 16:40:19 +0300

    Create jz1.js

---
+++ jz1.js
@@ -0,0 +1,4 @@
+$("<div class='fr borderg' style='padding:2px;background-color:white;float:left;margin-left:4px;'> <a href='https://mskchat.com/'>شات</a> <a href='https://mskchat.com/'>شات قلوب</a> <a href='https://www.al3nood.net/'>شات الحب</a> <a href='https://www.al3nood.net/'>شات تعب</a></div>").insertAfter($(".loginstat"));
+$(`<style>
+.fr.borderg,.fr.borderg a{font-size:10px!important;padding:0 5px!important;position:relative;color:#}.fr.borderg{border:0;margin-top:0;height:0}.fr.borderg{border:0;margin-top:0;height:0}
+</style>`).insertBefore('body');
